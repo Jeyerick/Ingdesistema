ac34967c8d9f2c7fc20e26c96444481a67089265
jeyerick <jeyerick1@gmail.com>
2023-03-13 15:04:57 -0500

    updated

--- su-proyecto/index.js
+++ su-proyecto/index.js
@@ -108,3 +108,15 @@ function loadHtml(){
  function clearHtml(){
     containerBuyCart.innerHTML = '';
  }
+
+ function iniciarMap(){
+    var coord = {lat:6.2501065 ,lng: -75.5709349,17};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
